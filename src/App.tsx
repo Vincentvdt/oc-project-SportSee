36f4d10026e490678216a6272b48ae2097636606
vincentvdt <vincent.vidot3@gmail.com>
2025-06-21 22:32:45 +0200

    Remove dashboard, API endpoints, charts, and associated assets as part of cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/layout/Header.tsx'
-import Sidebar from './components/layout/Sidebar.tsx'
-import styled from 'styled-components'
-import Dashboard from './components/layout/Dashboard.tsx'
 
-const Layout = styled.div`
-  display: flex;
-  flex-direction: row;
-  width: 100%;
-  min-height: fit-content;
-  height: calc(100dvh - 91px);
-  background: white;
-    color: #222;
-`
+function App() {
+  const [count, setCount] = useState(0)
 
-const App = () => {
   return (
     <>
-      <Header />
-      <Layout>
-        <Sidebar />
-        <Dashboard />
-      </Layout>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'node:path'
-import vercel from "vite-plugin-vercel"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vercel()],
-  vercel: {
-    defaultMaxDuration: 30,
-    expiration: 43200,
-  },
+  base: '/',
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
